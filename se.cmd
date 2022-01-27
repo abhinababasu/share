92786a1c1765ef358c1fd77e139ba7aab1b167f3
Abhinaba Basu <abhinaba@live.com>
2022-01-27 00:22:56 -0800

    Update se.cmd

--- se.cmd
+++ se.cmd
@@ -4,7 +4,7 @@ echo Running %USERNAME% private env
 
 set SCRIPT_DIR=%~dp0
 
-if exist %SCRIPT_DIR%\\Alias.txt (
+if exist %SCRIPT_DIR%\Alias.txt (
     SET MYSTUFF=%SCRIPT_DIR%
     goto ProbeDone
 )
@@ -19,24 +19,14 @@ if exist d:\OneDrive\bin\Alias.txt (
     goto ProbeDone
 )
 
-if exist d:\Skydrive\bin\Alias.txt (
-    SET MYSTUFF=d:\Skydrive
-    goto ProbeDone
-)
-
-if exist c:\Skydrive\bin\Alias.txt (
-    SET MYSTUFF=c:\Skydrive
-    goto ProbeDone
-)
-
 :ProbeDone
 echo Env folder is %MYSTUFF%
-SET ALIAS=%MYSTUFF%\bin\Alias.txt
+SET ALIAS=%MYSTUFF%\Alias.txt
 echo Alias is %Alias%
 
 doskey /macrofile=%ALIAS%
 SET path=%path%;%MYSTUFF%\bin;%MYSTUFF%\bin\SD
 SET SDFORMEDITOR=sdforms.exe
 
-color 4F
+color 1F
 
